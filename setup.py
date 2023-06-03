31387023bd728835236cd6236b86199e626b0272
liamcripwell <ljcripwell@gmail.com>
2023-06-03 17:51:07 +0200

    boilerplate files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sle",
+    version="0.0.1",
+    author="Liam Cripwell",
+    author_email="liam.cripwell@loria.fr",
+    description="Simplicity Level Estimate (SLE) metric implementation.",
+    packages=find_packages(),
+    include_package_data=True,
+)
